2905ede6c0a52acb38f759cb14ad725bcaee1226
Prince <prince07032004@gmail.com>
2025-11-27 22:42:09 +0530

    PrintNumber

---
+++ PrintingNumber.java
@@ -0,0 +1,8 @@
+public class PrintingNumber{
+    public static void main(String [] args){
+        int n=10;
+        for(int i=1;i<=n;i++){
+            System.out.print(" Number is:"+" "+i);
+        }
+    }
+}

5c292c0773ead05c1f066aeab6b981aebe5c5e8b
Prince <prince07032004@gmail.com>
2025-11-28 14:41:10 +0530

    CheckEvenOrOdd

---
+++ CheckEvenOrOdd.java
@@ -0,0 +1,12 @@
+public class CheckEvenOrOdd{
+    public static void main(String [] args){
+        int n=19;
+        
+        if(n%2==0){
+            System.out.println("Number is even:");
+        }
+        else{
+            System.out.println("Number is odd:");
+        }
+    }
+}

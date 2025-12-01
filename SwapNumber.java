64236031726f40305a7305fd831a8f66e6f31e5a
Prince <prince07032004@gmail.com>
2025-12-01 21:36:40 +0530

    SwapNumber

---
+++ SwapNumber.java
@@ -0,0 +1,15 @@
+public class SwapNumber{
+    public static void main(String [] args){
+        int a=10;
+        int b=20;
+        System.out.println("Before Swapping a is:"+ a);
+        System.out.println("Before Swapping b is:"+ b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapping a is:"+ a);
+        System.out.println("After Swapping b is:"+ b);
+
+
+    }
+}

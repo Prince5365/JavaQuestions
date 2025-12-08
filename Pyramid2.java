7224167ce1e73b5ba44a954d5dd1ddf3599c082b
Prince <prince07032004@gmail.com>
2025-12-08 23:28:41 +0530

    Pyramid2

---
+++ Pyramid2.java
@@ -0,0 +1,14 @@
+public class Pyramid2{
+    public static void main(String [] args){
+        for(int r=1;r<=5;++r){
+            for(int c=1;c<=9;++c){
+                if(c>=r && c<=10-r)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}

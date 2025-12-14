c2cb2e5eb1bd674bb42f31e1b63187b9dff08a45
Prince <prince07032004@gmail.com>
2025-12-14 23:19:26 +0530

    ProductOfArray

---
+++ ProductOfArray.java
@@ -0,0 +1,10 @@
+public class ProductOfArray{
+    public static void main(String [] args){
+        int arr[]={2,3,4,5};
+        int prod=1;
+        for(int i=0;i<arr.length;i++){
+            prod=prod*arr[i];
+        }
+        System.out.println(prod);
+    }
+}

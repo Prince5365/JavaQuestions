510077063b04ca3abee0b951c818a57bc53e69b5
Prince <prince07032004@gmail.com>
2026-01-08 23:57:42 +0530

    Minimum

---
+++ Minimum.java
@@ -0,0 +1,12 @@
+public class Minimum{
+    public static void main(String [] args){
+        int arr[]={1,2,7,8};
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+}
+                System.out.println(min);
+
+    }
+}

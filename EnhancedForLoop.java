9559acec394c20f998be9424034d422fe5962124
Prince <prince07032004@gmail.com>
2026-01-15 23:33:29 +0530

    Enhanced

---
+++ EnhancedForLoop.java
@@ -0,0 +1,8 @@
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        for (char vowel : vowels) {
+            System.out.println("Vowel: " + vowel);
+        }
+    }
+}
